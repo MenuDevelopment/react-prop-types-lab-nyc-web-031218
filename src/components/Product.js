f63b755ae97a0cb3ba647079490b53e9dbf59cd7
Manuel Grullon <astermanuelg@gmail.com>
2018-05-15 17:27:00 -0400

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,67 @@
-// Code Product Component Here
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Product extends React.Component{
+
+  render(){
+    return (
+      <div>
+        <h2>{this.props.name} </h2>
+        <ul>
+          <li>{this.props.producer}</li>
+          <li>{this.props.hasWatermark}</li>
+          <li>{this.props.color}</li>
+          <li>{this.props.weight}</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false
+}
+
+function properWeight (weight) {
+  return weight >= 80 && weight <= 300
+}
+
+// function customProp (props, weight, Product) {
+//   debugger
+//   if (!props[weight] || !properWeight(props[weight]) || !(typeof props[weight] === "number")) {
+//     return new Error(
+//       'Invalid prop `' + weight + '` supplied to' +
+//       ' `' + Product + '`. Validation failed.'
+//     );
+//   }
+// }
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white','eggshell-white','salmon']).isRequired,
+  weight: (props, propName) => {
+    if (!props[propName]) {
+      return new Error(
+        'Invalid prop ' + propName + ' supplied 1'
+      )
+    }
+    if (isNaN( props[propName])) {
+      return new Error(
+        'Invalid prop ' + propName + ' supplied 3'
+      )
+    }
+    if (!properWeight(props[propName])){
+      return new Error(
+        'Invalid prop ' + propName + ' supplied 2 '
+      )
+    }
+      return null
+  }
+
+}
+
+
+
+export default Product
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(
     name="Dunder Mifflin"
     producer="PaperCo"
     color="white"
-    weight={210}
+    weight={2000}
   />,
   document.getElementById('global')
 );
